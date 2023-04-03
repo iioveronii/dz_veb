a55689fdd280c19b0a638c38db8f619a9eea57fa
iioveronii <129761967+iioveronii@users.noreply.github.com>
2023-04-03 18:44:12 +0500

    Create spravochnik.php

---
+++ spravochnik.php
@@ -0,0 +1,91 @@
+<html>
+    <head>
+        <meta charset="utf8">
+    </head>
+ <style type="text/css">
+    body {
+    font-family: cursive;
+    background-color: #7B68EE;
+    font-size: 18px;
+    }
+    .form {
+	max-width: 100%;
+	padding: 0px 10px 10px 10px;
+	background: #f4f7f8;
+	margin: 5px 5px 5px 5px;
+	padding: 1em;
+	background: #f4f7f8;
+	border-radius: 8px;
+	font-family: "Lucida Sans Unicode", "Lucida Grande", sans-serif;
+    }
+    .form text {
+	font-family: "Lucida Sans Unicode", "Lucida Grande", sans-serif;
+	background: rgba(255,255,255,.1);
+	border: none;
+	border-radius: 4px;
+	font-size: 15px;
+	margin: 0;
+	outline: 0;
+	padding: 5px;
+	width: 100%;
+	box-sizing: border-box; 
+	-webkit-box-sizing: border-box;
+	-moz-box-sizing: border-box; 
+	background-color: #e8eeef;
+	color:#8a97a0;
+	-webkit-box-shadow: 0 1px 0 rgba(0,0,0,0.03) inset;
+	box-shadow: 0 1px 0 rgba(0,0,0,0.03) inset;
+	margin-bottom: 10px;
+}
+   
+   .btn {
+	display: inline-block;	
+	box-sizing: border-box;
+	padding: 0 25px;
+	margin: 0 15px 15px 0;
+	outline: none;
+	border: 1px solid #fff;
+	border-radius: 50px;
+	height: 46px;
+	line-height: 46px;
+	font-size: 16px;
+	font-weight: 600;
+	text-decoration: none;
+	color: #444;
+	background-color: #fff;
+	box-shadow: 0 4px 6px rgb(65 132 144 / 10%), 0 1px 3px rgb(0 0 0 / 8%);
+	cursor: pointer;
+	user-select: none;
+	appearance: none;
+	touch-action: manipulation;  
+	vertical-align: top;
+	transition: box-shadow 0.2s;
+    }
+    .btn:focus-visible {
+	border: 1px solid #4c51f9;
+	outline: none;
+    }
+    .btn:hover {
+	transition: all 0.2s;
+	box-shadow: 0 7px 14px rgb(65 132 144 / 10%), 0 3px 6px rgb(0 0 0 / 8%);
+    }
+    .btn:active {
+	background-color: #808080;
+    }
+    .btn:disabled {
+	background-color: #eee;
+	border-color: #eee;
+	color: #444;
+	cursor: not-allowed;
+    }
+    </style>
+<body>
+    <div class="form">
+    <form name="test1" method="post">
+    <li> <a href='vivod2.php'>Просмотр всех вопросов</a></li>
+    <li> <a href='poisk2.php'>Поиск понятий</a></li>
+    <li> <a href='vstavka2.php'>Вставка понятий</a></li>
+    </div>
+    </form>
+</body>
+</html>
